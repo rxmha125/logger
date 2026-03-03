d98e5731d819a0dec265b420626e07f16bcb48d7
rxmha125 <rx.mha2@gmail.com>
2026-03-03 21:58:37 +0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // === Basic security: change this to a strong secret ===
-const SECRET_KEY = 'your-super-secret-key-12345-change-me';  // ← CHANGE THIS NOW!
+const SECRET_KEY = 'Kj#9mP$vQ2xL&8rT!wY5nZ@eF3cA7bG+hM0kU4pR^sD6jNqX1tV*yB';  // ← CHANGE THIS NOW!
 
 // SQLite setup (file will be created automatically)
 const db = new sqlite3.Database('./database.db', (err) => {
